4e314c1ec010dd7a07d4735babcce2d138665f9b
Paritosh Jha <paritoshjha0504@gmail.com>
2024-07-15 09:26:44 +0530

    Added basic layout to profile screen

--- lib/features/profile/screens/profile.dart
+++ lib/features/profile/screens/profile.dart
@@ -1,5 +1,7 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/material.dart';
+import 'package:yumster/core/constants/constants.dart';
+import 'package:yumster/data/providers/user_provider.dart';
 
 class ProfileScreen extends ConsumerStatefulWidget {
   const ProfileScreen({super.key});
@@ -11,8 +13,94 @@ class ProfileScreen extends ConsumerStatefulWidget {
 class _ProfileScreenState extends ConsumerState<ProfileScreen> {
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Profile Screen'),
+    final user = ref.watch(userProvider);
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.center,
+      children: [
+        const SizedBox(
+          height: 20,
+        ),
+        CircleAvatar(
+          radius: MediaQuery.of(context).size.width * 0.12,
+          child: Image.asset(Constants.kUserAvatarPath),
+        ),
+        const SizedBox(
+          height: 10,
+        ),
+        Text(
+          user.username,
+          style: const TextStyle(
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        const SizedBox(
+          height: 30,
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: [
+            Column(
+              children: [
+                Text(
+                  user.posts.length.toString(),
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const Text(
+                  'Posts',
+                  style: TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+              ],
+            ),
+            Column(
+              children: [
+                Text(
+                  user.starredPosts.length.toString(),
+                  style: const TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                const Text(
+                  'Starred',
+                  style: TextStyle(
+                    fontSize: 14,
+                  ),
+                ),
+              ],
+            )
+          ],
+        ),
+        const SizedBox(
+          height: 20,
+        ),
+        Container(
+          alignment: Alignment.center,
+          margin: const EdgeInsets.symmetric(horizontal: 10),
+          padding: const EdgeInsets.all(8),
+          width: double.infinity,
+          decoration: BoxDecoration(
+            color: Colors.grey.shade200,
+            borderRadius: BorderRadius.circular(12),
+          ),
+          child: Text(
+            'Edit Profile',
+            style: Theme.of(context).textTheme.bodyLarge!.copyWith(
+                  fontWeight: FontWeight.bold,
+                ),
+          ),
+        ),
+        Divider(
+          thickness: 1,
+          height: 30,
+          color: Colors.grey.shade300,
+        ),
+      ],
     );
   }
 }
