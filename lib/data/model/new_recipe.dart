100ffd51dbd7a8508a5b995fe2b3b79953edf93b
Paritosh Jha <paritoshjha0504@gmail.com>
2024-07-19 08:50:01 +0530

    Fixed meal type in new_recipe model

--- lib/data/model/new_recipe.dart
+++ lib/data/model/new_recipe.dart
@@ -1,4 +1,9 @@
-class NewRecipe {
+// ignore_for_file: public_member_api_docs, sort_constructors_first
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+
+class NewRecipeModel {
   final String title;
   final String description;
   final List<String> ingredients;
@@ -6,10 +11,10 @@ class NewRecipe {
   final int duration;
   final String imageUrl;
   final List<String> tags;
-  final List<String> mealType;
+  final String mealType;
   final bool veg;
 
-  NewRecipe({
+  NewRecipeModel({
     required this.title,
     required this.description,
     required this.ingredients,
@@ -20,4 +25,100 @@ class NewRecipe {
     required this.mealType,
     required this.veg,
   });
+
+  NewRecipeModel copyWith({
+    String? title,
+    String? description,
+    List<String>? ingredients,
+    List<String>? steps,
+    int? duration,
+    String? imageUrl,
+    List<String>? tags,
+    String? mealType,
+    bool? veg,
+  }) {
+    return NewRecipeModel(
+      title: title ?? this.title,
+      description: description ?? this.description,
+      ingredients: ingredients ?? this.ingredients,
+      steps: steps ?? this.steps,
+      duration: duration ?? this.duration,
+      imageUrl: imageUrl ?? this.imageUrl,
+      tags: tags ?? this.tags,
+      mealType: mealType ?? this.mealType,
+      veg: veg ?? this.veg,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return <String, dynamic>{
+      'title': title,
+      'description': description,
+      'ingredients': ingredients,
+      'steps': steps,
+      'duration': duration,
+      'imageUrl': imageUrl,
+      'tags': tags,
+      'mealType': mealType,
+      'veg': veg,
+    };
+  }
+
+  factory NewRecipeModel.fromMap(Map<String, dynamic> map) {
+    return NewRecipeModel(
+      title: map['title'] as String,
+      description: map['description'] as String,
+      ingredients: List<String>.from(
+        (map['ingredients'] as List<String>),
+      ),
+      steps: List<String>.from(
+        (map['steps'] as List<String>),
+      ),
+      duration: map['duration'] as int,
+      imageUrl: map['imageUrl'] as String,
+      tags: List<String>.from(
+        (map['tags'] as List<String>),
+      ),
+      mealType: map['mealType'] as String,
+      veg: map['veg'] as bool,
+    );
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory NewRecipeModel.fromJson(String source) =>
+      NewRecipeModel.fromMap(json.decode(source) as Map<String, dynamic>);
+
+  @override
+  String toString() {
+    return 'NewRecipeModel(title: $title, description: $description, ingredients: $ingredients, steps: $steps, duration: $duration, imageUrl: $imageUrl, tags: $tags, mealType: $mealType, veg: $veg)';
+  }
+
+  @override
+  bool operator ==(covariant NewRecipeModel other) {
+    if (identical(this, other)) return true;
+
+    return other.title == title &&
+        other.description == description &&
+        listEquals(other.ingredients, ingredients) &&
+        listEquals(other.steps, steps) &&
+        other.duration == duration &&
+        other.imageUrl == imageUrl &&
+        listEquals(other.tags, tags) &&
+        other.mealType == mealType &&
+        other.veg == veg;
+  }
+
+  @override
+  int get hashCode {
+    return title.hashCode ^
+        description.hashCode ^
+        ingredients.hashCode ^
+        steps.hashCode ^
+        duration.hashCode ^
+        imageUrl.hashCode ^
+        tags.hashCode ^
+        mealType.hashCode ^
+        veg.hashCode;
+  }
 }
