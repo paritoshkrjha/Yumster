2e979e018562cf5cd9fb1f7d8378570100c803d0
Paritosh Jha <paritoshjha0504@gmail.com>
2024-07-20 17:49:39 +0530

    Configured firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,15 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:yumster/core/themes/themes.dart';
+import 'package:yumster/firebase_options.dart';
 import 'package:yumster/router.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const ProviderScope(child: MyApp()));
 }
 
